dc96a437f1c010f5d227cafcf1dde6ffd5ccecd0
Ondrej Kelle <tondrej@gmail.com>
2018-08-14 08:36:48 +0200

    Add AddRef/Release helper methods to TNativeObject

--- src/ChakraCoreClasses.pas
+++ src/ChakraCoreClasses.pas
@@ -327,7 +327,9 @@ type
     constructor Create(Args: PJsValueRefArray = nil; ArgCount: Word = 0; AFinalize: Boolean = False); virtual;
     destructor Destroy; override;
 
+    function AddRef: Integer;
     class procedure Project(const AName: UnicodeString = ''; UseStrictRules: Boolean = True);
+    function Release: Integer;
 
     property Context: TChakraCoreContext read GetContext;
     property ContextHandle: JsContextRef read GetContextHandle;
@@ -1516,6 +1518,11 @@ begin
   inherited;
 end;
 
+function TNativeObject.AddRef: Integer;
+begin
+  ChakraCoreCheck(JsAddRef(FTargetInstance, @Result));
+end;
+
 class procedure TNativeObject.Project(const AName: UnicodeString; UseStrictRules: Boolean);
 var
   ConstructorName: UnicodeString;
@@ -1529,6 +1536,11 @@ begin
   JsSetProperty(JsGlobal, ConstructorName, ConstructorFunc, UseStrictRules);
   RegisterPrototype;
   ChakraCoreCheck(JsSetPrototype(ConstructorFunc, Prototype));
+end;
+
+function TNativeObject.Release: Integer;
+begin
+  ChakraCoreCheck(JsRelease(FTargetInstance, @Result));
 end;
 
 end.

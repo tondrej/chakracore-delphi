bdbb5bcc49aabfd718642b5529826d988c780f15
Ondrej Kelle <tondrej@ondrejkelle.net>
2020-01-01 10:43:43 +0100

    Bump copyright

--- tests/ChakraCoreTests.dpr
+++ tests/ChakraCoreTests.dpr
@@ -2,7 +2,7 @@
 
 MIT License
 
-Copyright (c) 2019 Ondrej Kelle
+Copyright (c) 2020 Ondrej Kelle
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal

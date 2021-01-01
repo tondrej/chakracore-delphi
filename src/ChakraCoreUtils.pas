3e266c016466e5e9d68eb378506656c93e7f9abd
Ondrej Kelle <tondrej@ondrejkelle.net>
2021-01-01 20:21:22 +0100

    Bump copyright

--- src/ChakraCoreUtils.pas
+++ src/ChakraCoreUtils.pas
@@ -2,7 +2,7 @@
 
 MIT License
 
-Copyright (c) 2020 Ondrej Kelle
+Copyright (c) 2021 Ondrej Kelle
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
